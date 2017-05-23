e868d8ef9aa8c52628b5486ee0c818d17a1d539f
migmira <migmira@hotmail.com>
2017-05-23 00:40:17 +0000

    addding first product

---
+++ js/load.js
@@ -0,0 +1,22 @@
+$(function(){
+    
+    $("header").load('header.html')
+    
+    $("footer").load("footer.html"); 
+});
+
+
+
+// var request = new XMLHttpRequest();
+
+// request.open('GET', 'footer.html', true);
+
+// request.onload = function() {
+//   if (request.status >= 200 && request.status < 400) {
+//     var resp = request.responseText;
+
+//     document.querySelector('footer').innerHTML = resp;
+//   }
+// };
+
+// request.send();
