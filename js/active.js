7f42f36fe5ead820d1c1aef42fbaa1d9aa23daaa
migmira <migmira@hotmail.com>
2017-05-25 00:53:56 +0000

    added product 2,3,4 cctv

--- js/active.js
+++ js/active.js
@@ -4,18 +4,17 @@ $( document ).ready(function() {
 	console.log(path)
 	
 	var temp = $('#productos')
-	if (temp.length){
-		console.log(temp);
-		console.log("exitse");
-	}
-	else
-		console.log("no");
+	// if (temp.length){
+	// 	console.log(temp);
+	// 	console.log("exitse");
+	// }
+	// else
+	// 	console.log("no");
 	
 	switch( path ) {
 		case '/migmira/estmexico/cctv.html':
 			$("#productos").addClass("active");
 	        $("#cctv").addClass('active');
-	        console.log("entro");
 			break;
 	    case '/migmira/estmexico/cctv-kit1.html':
 	        $("#productos").addClass("active");
