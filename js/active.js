ebde3d316dcffdba2943c4de3cf926413622292e
Miguel Windows <migmira@hotmail.com>
2017-06-03 13:51:09 -0500

    Fixed active pages

--- js/active.js
+++ js/active.js
@@ -1,13 +1,34 @@
 // $(function(){
 $( document ).ready(function() {
 	var path = window.location.pathname;
-	console.log(path)
+	console.log(path);
 	
 	function cctv(){
 		$("#productos").addClass("active");
         $("#cctv").addClass('active');
 	}
-	
+
+	function alarmas(){
+		$("#productos").addClass("active");
+        $("#alarmas").addClass('active');
+	}
+
+	function videoporteros(){
+		$("#productos").addClass("active");
+		$("#video").addClass('active');
+	}
+
+	function gps(){
+		$("#productos").addClass("active");
+		$("#gps").addClass('active');
+	}
+
+	function acceso(){
+		$("#productos").addClass("active");
+		$("#acceso").addClass('active');
+	}
+
+
     $(document).on('DOMNodeInserted', function(e) {
 
         // console.log(e);
@@ -34,8 +55,26 @@ $( document ).ready(function() {
 			case '/cctv-kit6.html':
 				cctv();
 				break;
-			case '/cctv-kit7.html':
-				cctv();
+			case '/acceso.html':
+				acceso();
+				break;
+			case '/video.html':
+				videoporteros();
+				break;
+			case '/alarmas.html':
+				alarmas();
+				break;
+			case '/gps.html':
+				gps();
+				break;
+			case '/contacto.html':
+				$("#contacto").addClass('active');
+				break;
+			case '/nosotros.html':
+				$("#nosotros").addClass('active');
+				break;
+			case '/preguntas.html':
+				$("#preguntas").addClass('active');
 				break;
 
 			default:
@@ -46,4 +85,4 @@ $( document ).ready(function() {
 
 	});
 	
-})
+});
