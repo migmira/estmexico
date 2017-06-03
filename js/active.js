33b44e304b5f4b8bea3d65d15ac9bcaf49f02808
migmira <migmira@hotmail.com>
2017-06-03 18:24:27 +0000

    added favicon and stuf

--- js/active.js
+++ js/active.js
@@ -12,52 +12,6 @@ $( document ).ready(function() {
 
         // console.log(e);
    
-		// switch( path ) {
-		// 	case '/migmira/estmexico/cctv.html':
-		// 		cctv();
-		// 		break;
-		//     case '/migmira/estmexico/cctv-kit1.html':
-		// 		cctv();
-		//         break;
-		//     case '/migmira/estmexico/cctv-kit2.html':
-		// 		cctv();
-		//         break;
-		//
-		//     default:
-		//     	break;
-		// }
-
-
-		// switch( path ) {
-		// 	case '/estmexico/cctv.html':
-		// 		cctv();
-		// 		break;
-		// 	case '/estmexico/cctv-kit1.html':
-		// 		cctv();
-		// 		break;
-		// 	case '/estmexico/cctv-kit2.html':
-		// 		cctv();
-		// 		break;
-		// 	case '/estmexico/cctv-kit3.html':
-		// 		cctv();
-		// 		break;
-		// 	case '/estmexico/cctv-kit4.html':
-		// 		cctv();
-		// 		break;
-		// 	case '/estmexico/cctv-kit5.html':
-		// 		cctv();
-		// 		break;
-		// 	case '/estmexico/cctv-kit6.html':
-		// 		cctv();
-		// 		break;
-		// 	case '/estmexico/cctv-kit7.html':
-		// 		cctv();
-		// 		break;
-        //
-		// 	default:
-		// 		break;
-		// }
-
 		switch( path ) {
 			case '/cctv.html':
 				cctv();
